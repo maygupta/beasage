c72440028c2ec1878e9ba2f7c0eaeda292d1ba84
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-12-06 13:08:21 -0800

    added slokas

--- app/src/main/java/com/example/mayank/beasage/Home.java
+++ app/src/main/java/com/example/mayank/beasage/Home.java
@@ -2,9 +2,12 @@ package com.example.mayank.beasage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +22,7 @@ public class Home extends AppCompatActivity implements WheelPicker.OnItemSelecte
     int currentDurationPos = 2;
     int currentCountPos = 2;
     int curBookPos = 2;
+    boolean isPage = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,9 +69,14 @@ public class Home extends AppCompatActivity implements WheelPicker.OnItemSelecte
 
         setResultView();
 
-//        FrameLayout.LayoutParams flParams = new FrameLayout.LayoutParams
-//                (ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        flContainer.addView(wheelPicker, flParams);
+        Switch pageSlokaSwitch = (Switch) findViewById(R.id.switch1);
+        pageSlokaSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isPage = !isChecked;
+                setResultView();
+            }
+        });
 
     }
 
@@ -87,11 +96,19 @@ public class Home extends AppCompatActivity implements WheelPicker.OnItemSelecte
         setResultView();
     }
 
+    private String getColoredSpanned(String text, String color) {
+        String input = "<font color=" + color + ">" + text + "</font>";
+        return input;
+    }
+
     private void setResultView() {
         TextView tvBook = (TextView) findViewById(R.id.tvBook);
         tvBook.setText(books.get(curBookPos));
         TextView tvDuration = (TextView) findViewById(R.id.tvDuration);
-        tvDuration.setText(String.format(" %d %s", currentCountPos + 1, getDurationByPos()));
+        String in = getColoredSpanned("in", "#FFCC33");
+        String duration = getColoredSpanned(String.format(" %d %s", currentCountPos + 1, getDurationByPos()), "#fd951b1b");
+        String pleaseRead = getColoredSpanned("please read", "#FFCC33");
+        tvDuration.setText(Html.fromHtml(in + " " + duration + " " + pleaseRead));
 
         int sbPages = 15119;
         int bgPages = 868;
@@ -110,53 +127,118 @@ public class Home extends AppCompatActivity implements WheelPicker.OnItemSelecte
         int tlSlokas = 0;
         int isSlokas = 19;
 
-        int selScripturePages = 0;
-        switch(curBookPos) {
-            case 0:
-                selScripturePages = bgPages;
-                break;
-            case 1:
-                selScripturePages = sbPages;
-                break;
-            case 2:
-                selScripturePages = ccPages;
-                break;
-            case 3:
-                selScripturePages = krPages;
-                break;
-            case 4:
-                selScripturePages = isPages;
-                break;
-            case 5:
-                selScripturePages = ndPages;
-                break;
-            case 6:
-                selScripturePages = tlPages;
-                break;
-            case 7:
-                selScripturePages = niPages;
-                break;
-            default: break;
-        }
-
-        int numPagesDay = 0;
-        switch(currentDurationPos) {
-            case 0:
-                numPagesDay = selScripturePages / (currentCountPos + 1);
-                break;
-            case 1:
-                numPagesDay = selScripturePages / (7 * (currentCountPos + 1));
-                break;
-            case 2:
-                numPagesDay = selScripturePages / (30 * (currentCountPos + 1));
-                break;
-            case 3:
-                numPagesDay = selScripturePages / (365 * (currentCountPos + 1));
-                break;
-        }
-
         TextView tvResult = (TextView) findViewById(R.id.tvResult);
-        tvResult.setText(String.format("%d", numPagesDay));
+
+        if(isPage) {
+            int selScripturePages = 0;
+            switch (curBookPos) {
+                case 0:
+                    selScripturePages = bgPages;
+                    break;
+                case 1:
+                    selScripturePages = sbPages;
+                    break;
+                case 2:
+                    selScripturePages = ccPages;
+                    break;
+                case 3:
+                    selScripturePages = krPages;
+                    break;
+                case 4:
+                    selScripturePages = isPages;
+                    break;
+                case 5:
+                    selScripturePages = ndPages;
+                    break;
+                case 6:
+                    selScripturePages = niPages;
+                    break;
+                case 7:
+                    selScripturePages = tlPages;
+                    break;
+                default:
+                    break;
+            }
+
+            double numPagesDay = 0;
+            switch (currentDurationPos) {
+                case 0:
+                    numPagesDay = selScripturePages * 1.0 / (currentCountPos + 1);
+                    break;
+                case 1:
+                    numPagesDay = selScripturePages * 1.0 / (7 * (currentCountPos + 1));
+                    break;
+                case 2:
+                    numPagesDay = selScripturePages * 1.0 / (30 * (currentCountPos + 1));
+                    break;
+                case 3:
+                    numPagesDay = selScripturePages * 1.0 / (365 * (currentCountPos + 1));
+                    break;
+            }
+
+            if(numPagesDay < 1) {
+                numPagesDay = 1;
+            }
+            numPagesDay = Math.ceil(numPagesDay);
+            tvResult.setText(String.format("%d", Math.round(numPagesDay)));
+            TextView pageOrSloka = (TextView) findViewById(R.id.pageOrSloka);
+            pageOrSloka.setText("pages");
+        } else {
+            int selScriptureSlokas = 0;
+            switch (curBookPos) {
+                case 0:
+                    selScriptureSlokas = bgSlokas;
+                    break;
+                case 1:
+                    selScriptureSlokas = sbSlokas;
+                    break;
+                case 2:
+                    selScriptureSlokas = ccSlokas;
+                    break;
+                case 3:
+                    selScriptureSlokas = krSlokas;
+                    break;
+                case 4:
+                    selScriptureSlokas = isSlokas;
+                    break;
+                case 5:
+                    selScriptureSlokas = ndSlokas;
+                    break;
+                case 6:
+                    selScriptureSlokas = niSlokas;
+                    break;
+                case 7:
+                    selScriptureSlokas = tlSlokas;
+                    break;
+                default:
+                    break;
+            }
+
+            double numSlokasDay = 0.0;
+            switch (currentDurationPos) {
+                case 0:
+                    numSlokasDay = selScriptureSlokas * 1.0 / (currentCountPos + 1);
+                    break;
+                case 1:
+                    numSlokasDay = selScriptureSlokas * 1.0 / (7 * (currentCountPos + 1));
+                    break;
+                case 2:
+                    numSlokasDay = selScriptureSlokas * 1.0 / (30 * (currentCountPos + 1));
+                    break;
+                case 3:
+                    numSlokasDay = selScriptureSlokas * 1.0 / (365 * (currentCountPos + 1));
+                    break;
+            }
+
+            if(selScriptureSlokas == 0) {
+                tvResult.setText("N/A");
+            } else {
+                numSlokasDay = Math.ceil(numSlokasDay);
+                tvResult.setText(String.format("%d", Math.round(numSlokasDay)));
+            }
+            TextView pageOrSloka = (TextView) findViewById(R.id.pageOrSloka);
+            pageOrSloka.setText("slokas");
+        }
     }
 
     private String getDurationByPos() {
