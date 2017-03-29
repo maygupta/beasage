156687a48771a2daa7df10fe9d9c13ce5f98d211
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2017-03-28 20:06:48 -0700

    improved UI

--- app/src/main/java/com/iskcon/isv/beasage/Books.java
+++ app/src/main/java/com/iskcon/isv/beasage/Books.java
@@ -15,13 +15,13 @@ public class Books {
     Books(){
         booksMap = new ArrayList<>();
         booksMap.add(new BookItem(0,"Bhagavad Gita", 717, 700,"https://media.licdn.com/mpr/mpr/p/5/005/096/3d1/242b35e.jpg"));
-        booksMap.add(new BookItem(1,"Srimad Bhagavatam", 14625, 14094, "http://bbtacademic.com/wp-content/uploads/2016/08/bhagavatam_big.jpg"));
-        booksMap.add(new BookItem(2,"Caitanya Caritamrta", 6624, 11555, "http://bbtacademic.com/wp-content/uploads/2016/01/Caitanya-caritamrta.jpg"));
-        booksMap.add(new BookItem(3,"Krsna Book", 796, 0,"http://www.prabhupada.org.uk/images/spb05.jpg"));
-        booksMap.add(new BookItem(4,"Sri Isopanishad", 138, 19, "http://bbtacademic.com/wp-content/uploads/2016/07/cover_iso_pb_0.png"));
-        booksMap.add(new BookItem(5,"Nectar of Devotion", 399, 0, "http://bbtacademic.com/wp-content/uploads/2016/07/the-nectar-of-devotion.jpg"));
-        booksMap.add(new BookItem(6,"Nectar of Instruction", 91, 11, "http://bbtacademic.com/wp-content/uploads/2016/07/Screen-Shot-2016-07-26-at-9.53.51-PM.png"));
-        booksMap.add(new BookItem(7,"TLC", 350, 0, "http://krishna.org/images/Misc/tlc_cover.jpg?x64805"));
+        booksMap.add(new BookItem(1,"Srimad Bhagavatam", 14625, 14094, "http://www.goldenagemedia.org/itemimages/ProductImage1/srimad-bhagavatam---complete-18-volumes-set-(eng).jpg"));
+        booksMap.add(new BookItem(2,"Caitanya Caritamrta", 6624, 11555, "http://korg.cdn.krishna.org/wp-content/uploads/2013/02/KA1_192.jpg?x64805"));
+        booksMap.add(new BookItem(3,"Krsna Book", 796, 0,"http://korg.cdn.krishna.org/wp-content/uploads/2011/11/Krishna-enjoying-with-His-friends.jpg?x64805"));
+        booksMap.add(new BookItem(4,"Sri Isopanishad", 138, 19, "https://i.ytimg.com/vi/AkU0WX-aCB0/hqdefault.jpg"));
+        booksMap.add(new BookItem(5,"Nectar of Devotion", 399, 0, "http://covers.globalbbt.org/sites/default/files/covers/EN_NOD_1975.jpg"));
+        booksMap.add(new BookItem(6,"Nectar of Instruction", 91, 11, "https://s-media-cache-ak0.pinimg.com/564x/b2/71/de/b271de222cfad5956f22f444852e7c29.jpg"));
+        booksMap.add(new BookItem(7,"TLC", 350, 0, "http://www.utahkrishnas.org/wp-content/uploads/2010/11/caitanya-nityananda.jpg"));
     }
 
     public static Books getBooksInstance() {
